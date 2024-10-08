a33dd0b917ab5f97bc604599b89beecc577a065b
Amir Babic <Amir.Babic@basistechnologies.com>
2024-10-08 13:45:09 +0200

    Update aws_glue_trigger.tf

--- aws_glue_trigger.tf
+++ aws_glue_trigger.tf
@@ -4,16 +4,10 @@ resource "aws_glue_trigger" "glue" {
   name     = each.key
   type     = each.value.Type
   schedule = each.value.Schedule
-
   start_on_creation = each.value.StartOnCreation
-
   actions {
     job_name = aws_glue_job.glue.name  # Replace with your actual Glue job name
     arguments = each.value.Actions.Arguments
     timeout   = each.value.Actions.Timeout
   }
-
-
-
-
 }
