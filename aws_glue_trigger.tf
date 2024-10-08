03c31f88326585bbbe4051626d8b4cecae7da9e9
Amir Babic <Amir.Babic@basistechnologies.com>
2024-10-08 14:41:01 +0200

    Update aws_glue_trigger.tf

--- aws_glue_trigger.tf
+++ aws_glue_trigger.tf
@@ -1,7 +1,7 @@
 resource "aws_glue_trigger" "glue" {
   for_each = var.glue-triggers
 
-  name     = each.key
+  name     = "${var.project-name}-${var.env}-${each.key}"
   type     = each.value.Type
   schedule = each.value.Schedule
   start_on_creation = each.value.StartOnCreation
